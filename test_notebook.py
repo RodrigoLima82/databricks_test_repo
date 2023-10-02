60cfe10a895ac24346a4e3af755004e24540b57c
rodrigolima82@gmail.com <rodrigolima82@gmail.com>
2023-10-02 13:47:51 +0000

    Adicionando novo codigo

--- test_notebook.py
+++ test_notebook.py
@@ -1,6 +1,16 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Teste
 
 # COMMAND ----------
 
 import random
 print(random.randint(0, 5))
+
+# COMMAND ----------
+
+print(random.randint(10, 50))
+
+# COMMAND ----------
+
+
